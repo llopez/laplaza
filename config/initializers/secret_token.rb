7f18125cba5a242774832e159cd97b921407cba0
Luis Lopez <luigibyte@gmail.com>
2014-04-24 23:32:53 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaPlaza::Application.config.secret_key_base = '5c33ca217b8835b1185f0797e775a876bb48741bd9f1e43450196f2d383c9fa4252317882f6b114bc7c9610bee6b983fcc3bcdee20a49883cb39867aadef3b03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaPlaza::Application.config.session_store :cookie_store, key: '_la_plaza_session'
