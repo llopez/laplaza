8e621b8b3038d4cacd07e3a60263253a7d97de45
Luis Lopez <luigibyte@gmail.com>
2014-04-29 22:27:43 -0300

    simple articles index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,8 @@
+class ArticlesController < ApplicationController
+  respond_to :html
+  
+  def index
+    @articles = Article.all
+    respond_with @articles
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 LaPlaza::Application.routes.draw do
+  resources :articles, only: :index
+  root to: 'articles#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
