267fb030a45dda8354aa9579cd66f64a250db399
Luis Lopez <luigibyte@gmail.com>
2014-04-29 23:41:40 -0300

    Articles:
      * Edit, View and Delete.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,31 @@ class ArticlesController < ApplicationController
     @articles = @q.result.page(params[:page]).per(10)
     respond_with @articles
   end
+  
+  def show
+    @article = Article.find params[:id]
+    respond_with @article
+  end
+  
+  def edit
+    @article = Article.find params[:id]
+    respond_with @article
+  end
+  
+  def update
+    @article = Article.find params[:id]
+    @article.update_attributes(article_params)
+    respond_with(@article)
+  end
+  
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+    redirect_to articles_path
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :price, :stock)
+  end
 end
