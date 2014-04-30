f5200afc69f0fbc925e9e129800242dcb6934ead
Luis Lopez <luigibyte@gmail.com>
2014-04-29 22:26:10 -0300

    articles seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create title: "Article 1", price: 10, stock: 23
+Article.create title: "Article 2", price: 3, stock: 5
+Article.create title: "Article 3", price: 4, stock: 2
+Article.create title: "Article 4", price: 1, stock: 1
+Article.create title: "Article 5", price: 8, stock: 50
