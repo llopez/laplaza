f46fc035bb3c45e21474b5476894df33225d1ea8
Luis Lopez <luigibyte@gmail.com>
2014-08-02 22:00:57 -0300

    sanitize title

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   validates :title, :price, :stock, presence: true
   validates :price, numericality: true, if: "price.present?"
   validates :stock, numericality: true, if: "stock.present?"
+
+  before_save do
+    self.title = self.title.gsub("  ", " ").strip
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "title sanitation" do
+    article = Article.new(title: " Accesorio pvc curva  a 90 ")
+    article.save
+    assert_equal "Accesorio pvc curva a 90", article.title
+  end
+
 end
